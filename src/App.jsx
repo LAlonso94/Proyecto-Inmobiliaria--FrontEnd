383f67b673129e08a7ce650492e7c6a0dc7ce850
alanz420 <lucasalonso.m94@gmail.com>
2022-12-02 10:52:51 -0300

    Agrego el boton de administrar

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,8 @@ import Home from "./routes/Home";
 function App() {
   return (
     <div>
-<<<<<<< HEAD
       <ChakraProvider>
         <Home />
-=======
-      {/* <Home /> */}
-      <ChakraProvider>
->>>>>>> 3ac4aaf1cfde03105e0b85d94ee51a7f8f9792d4
         <Login />
         <Register />
       </ChakraProvider>
