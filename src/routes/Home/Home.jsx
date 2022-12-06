17170610bfceb55a0106fbaf8d84a53a6d4b24bc
alanz420 <lucasalonso.m94@gmail.com>
2022-12-06 16:14:11 -0300

    arreglos de importacion

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -2,8 +2,6 @@ import React, { useEffect, useState } from "react";
 import Footer from "../../components/Footer/Footer";
 import Nav from "../../components/Nav/Nav";
 import Header from "../../components/Header/Header";
-import FormEstate from "../../components/FormEstate/FormEstate";
-import Formulario from "../../components/FormEstate/FormAdd";
 import Company from "../../components/Company/Company";
 import EstatesMap from "../../components/EstatesMap/EstatesMap";
 import Rule_Estates from "../../api/Rule_Estates";
